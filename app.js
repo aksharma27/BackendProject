e3dea94166e962fcd5a2cfaf4f20b4af9fab8e95
abhishek-dev <enquireabhishek@gmail.com>
2022-12-18 02:17:34 +0530

    basic server setup

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
+
 const app = express();
+
+
+app.get('/api/health', (req, res) =>{
+    res.send({
+        time: new Date(),
+        server: "Shuffle Backend",
+        status: "Active"
+    });
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`App listening on ${PORT}`);
+})
